9c0be98fda1d9b66230d648db416d5d3c0ef3e23
Konstantin Smirnov <sasharugby@yandex.ru>
2023-06-24 21:08:19 +0300

    good

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write($"{message} >");
+    string inputeStr = Console.ReadLine();
+    int value;
+    if ( int.TryParse(inputeStr, out value))
+    {
+        return value;
+    }
+    System.Console.WriteLine("Извините, но вы ввели не число");
+    Environment.Exit(0);
+    return 0;
+}
+int a = Prompt("Введите число");
+int b = Prompt("Введите число");
+int c =1;
+int r =1;
+while ( c<=b)
+{
+    r=r*a;
+    c=c+1;
+}
+System.Console.WriteLine(r);
